b933ead8fb27e26118aaa6d49b15fe0a9b039739
Isti <steven.trautmann1@gmail.com>
2020-11-30 00:11:47 +0100

    flipping and floating card functionality

--- AnimateCard.js
+++ AnimateCard.js
@@ -6,7 +6,32 @@ function cardAnimation(){
         container.addEventListener("mousemove", (e) => {
             let xAxis = ((container.clientWidth / 2 - (e.pageX-container.offsetLeft)) / 20);
             let yAxis = ((container.clientHeight / 2 - (e.pageY-container.offsetTop)) / 20);
-            card.style.transform = `rotateY(${xAxis}deg) rotateX(${-yAxis}deg)`
+            card.style.transform = `rotateY(${xAxis}deg) rotateX(${-yAxis}deg)`;
+        })
+
+        let rabbit = container.querySelector(".rabbit").querySelector("img");
+        container.addEventListener("mouseenter", (e) => {
+            card.style.transition = "none";
+            //animating the rabbit also
+            let prevTransform = rabbit.style.transform;
+            if (prevTransform.includes("rotateY(180deg)")){
+                rabbit.style.transform = "translateZ(2rem) rotateY(180deg)";
+            } else {
+                rabbit.style.transform = "translateZ(2rem)";
+            }
+        })
+
+        //on mouse leave, put back everything
+        container.addEventListener("mouseleave", (e) => {
+            card.style.transition = "0.75s ease"
+            card.style.transform = "rotateY(0) rotateX(0)";
+
+            let prevTransform = rabbit.style.transform;
+            if (prevTransform.includes("rotateY(180deg)")){
+                rabbit.style.transform = "translateZ(0) rotateY(180deg)";
+            } else {
+                rabbit.style.transform = "translateZ(0)";
+            }
         })
     }
 }
@@ -34,8 +59,30 @@ function rabbitPetting(){
         let rabbit = container.querySelector(".rabbit").querySelector("img");
 
         purchaseButton.addEventListener("click", (e) => {
+            let prevTransform = rabbit.style.transform;
             e.target.classList.toggle("active");
-            rabbit.classList.toggle("petting");
+            if (prevTransform.includes("rotateY(180deg)")){
+                rabbit.classList.toggle("pettingLifted");
+            } else {
+                rabbit.classList.toggle("pettingUnlifted");
+            }
+        })
+
+        rabbit.addEventListener("click", (e) => {
+            let prevTransform = rabbit.style.transform;
+            if (prevTransform.includes("rotateY(180deg)")){
+                rabbit.style.transform = prevTransform.replace("rotateY(180deg)", '');
+                if (rabbit.classList.contains("pettingLifted")){
+                    rabbit.classList.toggle("pettingLifted");
+                    rabbit.classList.toggle("pettingUnlifted");
+                }
+            } else {
+                rabbit.style.transform = prevTransform + " rotateY(180deg)";
+                if (rabbit.classList.contains("pettingUnlifted")){
+                    rabbit.classList.toggle("pettingUnlifted");
+                    rabbit.classList.toggle("pettingLifted");
+                }
+            }
         })
     }
 }
